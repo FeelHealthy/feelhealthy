1763eec7b05ab4ab2f8b41b729ffc405fc6f25af
Andrei Stashkvich <andrei.stashkevich@hansecom.com>
2024-01-03 12:53:44 +0100

    add popunder

---
+++ ads.js
@@ -0,0 +1,6 @@
+
+var uid = '471550';
+var wid = '709368';
+var pop_fback = 'up';
+var pop_tag = document.createElement('script');pop_tag.src='//cdn.popcash.net/show.js';document.body.appendChild(pop_tag);
+pop_tag.onerror = function() {pop_tag = document.createElement('script');pop_tag.src='//cdn2.popcash.net/show.js';document.body.appendChild(pop_tag)};
